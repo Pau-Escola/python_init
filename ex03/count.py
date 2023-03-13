7f2579626c65d229405ab6c56cce9f7ddc98f7fb
Pau Escola Barba <pescola-@cbr2s4.42barcelona.com>
2023-03-13 22:20:31 +0100

    Suuu

--- ex03/count.py
+++ ex03/count.py
@@ -1,10 +1,17 @@
 import string
 
 def text_analyzer(*args):
-    if(len(args) == 1):
-        try:
-            p = int(args[0])
-            print("Argument provided is not a String.")
+    p = ''
+    if(len(args) > 1):
+        print ("Too many arguments")
+    elif (len(args) == 0):
+        while (not p):
+            p = input("What should I do next? ")
+    else:
+        p = args[0]
+    try:
+        p = int(args[0])
+        print("Argument provided is not a String.")
         #for i in range(0, len(s) - 1):
             #t = s[i];
             #if (t.isupper()):
@@ -16,21 +23,23 @@ def text_analyzer(*args):
           #  else:
          #       punc_mark += 1
         #printf ("Lower letters: %i \n Caps: %i", lower_l, upper_l)
-        except:
-            print("Argument provided is a String.")
-            s = args[0]
-            upper_l = 0
-            lower_l = 0
-            spaces = 0
-            punc_mark = 0
-            for i in range(0, len(s)):
-                t = s[i]
-                if (t.isupper()):
-                    upper_l += 1
-                elif(t.islower()):
-                    lower_l += 1
-                elif(t == " "):
-                    spaces += 1
-                elif(t in string.punctuation):
-                    punc_mark += 1
-            print ("Lower letters: " + str(lower_l) +".\n" + "Upper letters: " + str(upper_l) + ".\n" + "Spaces: " + str(spaces) + ".\n" + "Punctuation mark: " + str(punc_mark) + ".")
+    except:
+        print("Argument provided is a String.")
+        s = str(p)
+        upper_l = 0
+        lower_l = 0
+        spaces = 0
+        punc_mark = 0
+        chars = 0
+        for i in range(0, len(s)):
+            t = s[i]
+            if (t.isupper()):
+                upper_l += 1
+            elif(t.islower()):
+                lower_l += 1
+            elif(t == " "):
+                spaces += 1
+            elif(t in string.punctuation):
+                punc_mark += 1
+            chars = i;
+        print ("Total characters: " + str(chars + 1) + ".\n" + "Lower letters: " + str(lower_l) +".\n" + "Upper letters: " + str(upper_l) + ".\n" + "Spaces: " + str(spaces) + ".\n" + "Punctuation mark: " + str(punc_mark) + ".")
