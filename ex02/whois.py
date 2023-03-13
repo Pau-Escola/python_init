04e5ba51d60a845451fbeb77d3306b2646352a52
Pau Escola Barba <pescola-@cbr2s4.42barcelona.com>
2023-03-13 19:39:49 +0100

    Ex02 rightfully working

--- ex02/whois.py
+++ ex02/whois.py
@@ -1,14 +1,10 @@
 import sys
 
 if (len(sys.argv) == 2):
-    if (sys.argv[1].isdigit()):
-        if (int(sys.argv[1]) == 0):
-            print("I am Zero.")
-        elif (int(sys.argv[1]) / 2 == 1 or int(sys.argv[1] == 1)):
-            print("I am Odd.")
-        else:
-            print("I am Even.")
-    else:
+    try:
+        n = int(sys.argv[1]);
+        print ("I am Zero.") if n == 0 else print("I am Odd.") if n % 2 == 1 or n == 1 else print("I am Even.")
+    except:
         print ("AssertionError : argument is not an integer")
 elif (len(sys.argv) > 2):
     print ("AssertionError: more than one argument are provided")
